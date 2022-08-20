4b5ee7adf984f9b71e5b19f96108f323762b32b8
Andrey Dmitriev <admit8490@gmail.com>
2022-08-20 23:31:42 +0200

    chore: general styling, todo-s: watch all files!

---
+++ script.js
@@ -0,0 +1,8 @@
+// Constants
+
+// here goes quotes object
+// and colors for random contaner bg
+
+// ??? figure out, is it necessary to use React for this one
+
+//first thing to render: random quote as soon as page loads
